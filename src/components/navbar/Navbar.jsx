1a4fdaa1e24c48609cc93a6e2c8952d4a624a52f
Vikas Talreja <vikazztalreja@gmail.com>
2025-05-03 15:50:50 +0530

    About us page update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -39,13 +39,7 @@ const Navbar = () => {
 
         {/* Desktop Menu */}
         <div className="hidden md:flex items-center space-x-8">
-          <Link 
-            href="/solutions" 
-            className="text-gray-700 hover:text-black transition-colors" 
-            style={{ fontFamily: 'Satoshi, sans-serif', fontWeight: 500 }}
-          >
-            Solutions
-          </Link>
+     
           
           <Link 
             href="/about" 
